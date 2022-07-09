172588e63a8376270adadbd8e7dcf697fced1d21
Neduthegoat <78829296+Neduthegoat@users.noreply.github.com>
2022-07-09 16:43:46 +0100

    initializing attributes of the class

--- project.py
+++ project.py
@@ -1,2 +1,4 @@
 #Creating Employee class
 class Employee:
+ #initializing attributes of the class
+    def __init__(self,StaffID,LastName,FirstName,RegHours,HourlyRate,OTMultiple,TaxCredit,StandardBand):
