bef34b96815d7b2a5b6a6988fe883a685cd3597e
Neduthegoat <78829296+Neduthegoat@users.noreply.github.com>
2022-07-09 16:49:03 +0100

    setting variables

--- project.py
+++ project.py
@@ -13,3 +13,28 @@ class Employee:
         self.StandardBand=StandardBand
     #Creating the `computePayment` method     
     def computePayment(self,TotalTime,Date):
+        #setting variables upfront to make the calculations more readable
+        if TotalTime > self.RegHours:
+            Reg_hours=self.RegHours
+            overtime_hours=TotalTime-self.RegHours
+        else:
+            Reg_hours=TotalTime
+            overtime_hours=0
+            
+        
+        overtime_rate=int(self.HourlyRate*self.OTMultiple)
+        regular_pay=self.HourlyRate*self.RegHours
+        overtime_pay=int(overtime_hours*overtime_rate)
+        gross_pay=overtime_pay+regular_pay
+        if gross_pay > self.StandardBand:
+            standard_pay=self.StandardBand
+        else:
+            standard_pay=gross_pay
+        higher_pay=gross_pay-standard_pay
+        standard_tax=int(standard_pay*0.2)
+        higher_tax=higher_pay*0.4
+        total_tax=standard_tax+higher_tax
+        net_tax=round(total_tax-self.TaxCredit,2)
+        prsi=0.04*gross_pay
+        net_deductions=round(net_tax+prsi,2)
+        net_pay=round(gross_pay-net_deductions,2)
